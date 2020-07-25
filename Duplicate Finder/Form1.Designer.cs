b100c5e77cef50b666f410d4fc02f53611ee75e4
Reza Parsian <rezaparsian76@gmail.com>
2020-07-25 13:12:28 +0430

    for now

---
+++ Duplicate Finder/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Duplicate_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncheck = new System.Windows.Forms.Button();
+            this.lstfiles = new System.Windows.Forms.ListBox();
+            this.lstdup = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btncheck
+            // 
+            this.btncheck.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btncheck.Location = new System.Drawing.Point(293, 228);
+            this.btncheck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(172, 28);
+            this.btncheck.TabIndex = 0;
+            this.btncheck.Text = "Find Duplicate";
+            this.btncheck.UseVisualStyleBackColor = true;
+            this.btncheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstfiles
+            // 
+            this.lstfiles.FormattingEnabled = true;
+            this.lstfiles.ItemHeight = 16;
+            this.lstfiles.Location = new System.Drawing.Point(9, 10);
+            this.lstfiles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstfiles.Name = "lstfiles";
+            this.lstfiles.Size = new System.Drawing.Size(456, 100);
+            this.lstfiles.TabIndex = 1;
+            this.lstfiles.DoubleClick += new System.EventHandler(this.lstfiles_DoubleClick);
+            // 
+            // lstdup
+            // 
+            this.lstdup.FormattingEnabled = true;
+            this.lstdup.ItemHeight = 16;
+            this.lstdup.Location = new System.Drawing.Point(9, 118);
+            this.lstdup.Margin = new System.Windows.Forms.Padding(4);
+            this.lstdup.Name = "lstdup";
+            this.lstdup.Size = new System.Drawing.Size(456, 100);
+            this.lstdup.TabIndex = 2;
+            this.lstdup.DoubleClick += new System.EventHandler(this.lstdup_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 266);
+            this.Controls.Add(this.lstdup);
+            this.Controls.Add(this.lstfiles);
+            this.Controls.Add(this.btncheck);
+            this.Font = new System.Drawing.Font("Neon 80s", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Duplicate_Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncheck;
+        private System.Windows.Forms.ListBox lstfiles;
+        private System.Windows.Forms.ListBox lstdup;
+    }
+}
+
---
+++ Duplicate Finder/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Duplicate_Finder
+{
+    public partial class Form1 : Form
+    {
+        List<string> items = new List<string>();
+        List<string> duplicate_items = new List<string>();
+        List<int> duplicate_index = new List<int>();
+
+        MD5 md5 = MD5.Create();
+
+        string[] files;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private string[] FethFiles(string dir)
+        {
+            return Directory.GetFiles(dir);
+        }
+
+        private string GetFileMD5(string file_address)
+        {
+            string result = "";
+
+            using (FileStream stream = File.OpenRead(file_address))
+            {
+                byte[] checksum = md5.ComputeHash(stream);
+                result = BitConverter.ToString(checksum).Replace("-", String.Empty).ToLower();
+            }
+
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dir = @"E:\Phone\Pic";
+
+            files = FethFiles(dir);
+
+            Task.Run(() =>
+            {
+                foreach (string item in files)
+                {
+                    items.Add(GetFileMD5(item));
+                    lstfiles.Items.Add(item);
+
+                    lstfiles.SelectedIndex = (lstfiles.Items.Count - 1);
+                }
+            });
+
+            duplicate_items = items.GroupBy(x => x).Where(g => g.Count() > 1).Select(y => y.Key).ToList();
+
+            duplicate_index = items.Select(x => duplicate_items.IndexOf(x)).ToList();
+
+            for (int i = 0; i < duplicate_index.Count; i++)
+            {
+                if (duplicate_index[i] != -1)
+                {
+                    lstdup.Items.Add(files[i]);
+                }
+            }
+
+        }
+
+        private void lstfiles_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(lstfiles.SelectedItem.ToString());
+        }
+
+        private void lstdup_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(lstdup.SelectedItem.ToString());
+        }
+    }
+}
